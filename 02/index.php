bc26d6069b8d82d93c53e686922604187f6c15ec
CORRADOLAMBERTI26 <200643637@student.georgianc.on.ca>
2026-01-14 16:04:56 -0500

    Going to update the lesson code once its uploaded and I added a personal note for the first lab. Just commiting it so all saves

--- 02/index.php
+++ 02/index.php
@@ -3,13 +3,45 @@
 
 //2. Code Commenting 
 
+// <--- Inline Commenting
+
+<?php
+/*  <-----
+
+Multi-line Commenting
+
+*/  
+
+
 
 //3. Variables, Data Tyoes, Concatenation, Conditional Statements & Echo
 
+$firstName = "Corrado"; // String
+$lastName = "Lamberti"; //String
+$age = 18; //int
+$isInstructor = true; // boolean
+
+echo "<p> Hello there, my name is " . $firstName . " " . $lastName . "</p>"
+
+if ($isInstructor) {
+    echo "<p> I am your teacher </p>";
+    
+}
+
+    else {
+        echo "<p> Opps, teach didn't show!"
+    }
 
 //4. Loosely Typed Language Demo
 
+$num1 = 1;``
+$num2 = "10";
 
+funtion add(int $num1, int $num2) ; int {
+    return $num1 + $num2;
+}
+
+echo "<p>" . add ($num1, $num2) . "</p>";
 //5. Strict Types & Types Hints
 
 
