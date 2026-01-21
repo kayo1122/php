13e1b676d800e0aeb184c9eab8a612862d8d24af
JessicaGilfillan <49848402+JessicaGilfillan@users.noreply.github.com>
2026-01-21 15:16:09 -0500

    add and move completed lab 1 files

--- assignments/labs/lab_one/index.php
+++ assignments/labs/lab_one/index.php
@@ -1,6 +1,4 @@
 <?php
-require "connect.php"; 
 require "header.php"; 
 echo "<p> Follow the instructions outlined in instructions.txt to complete this lab. Good luck & have fun!😀 </p>";
-require "car.php"; 
 require "footer.php"; 
